757fc96975cbd39fd95b03afcd95c099e845ae4f
JD <mi3ta@sent.as>
2022-07-13 04:49:18 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+/** Distance 2D.
+ *
+ * Move the mouse across the image to obscure and reveal the matrix.
+ * Measures the distance from the mouse to each square and sets the
+ * size proportionally.
+ */
+var max_distance;
+
+function setup() {
+    initializeFields();
+    createCanvas(displayWidth, displayHeight);
+    noStroke();
+    max_distance = dist(0, 0, width, height);
+}
+
+function draw() {
+    background(0);
+    for (var i = 0; i <= width; i += 20) {
+        for (var j = 0; j <= height; j += 20) {
+            var size = dist(mouseX, mouseY, i, j);
+            size = size / max_distance * 66;
+            ellipse(i, j, size, size);
+        }
+    }
+}
+
+function initializeFields() {
+    max_distance = 0;
+}
+
+
